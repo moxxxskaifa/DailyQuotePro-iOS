fd4a837904d44f17af6237fb72f5db8ef4f22de9
moxxxskaifa <moxxxskaifa@users.noreply.github.com>
2026-06-02 08:45:22 +0800

    Fix: add gradle wrapper + build workflow

---
+++ composeApp/src/commonMain/kotlin/com/dailyquote/app/App.kt
@@ -0,0 +1,5 @@
+package com.dailyquote.app
+import androidx.compose.runtime.Composable
+import com.dailyquote.app.ui.screens.MainScreen
+import com.dailyquote.app.ui.theme.AppTheme
+@Composable fun App() { AppTheme { MainScreen() } }
---
+++ composeApp/src/iosMain/kotlin/com/dailyquote/app/MainViewController.kt
@@ -0,0 +1,3 @@
+package com.dailyquote.app
+import androidx.compose.ui.window.ComposeUIViewController
+fun MainViewController() = ComposeUIViewController { App() }
